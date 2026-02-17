fa03ef9cad6cf5eb27454cbfc2a433f2d633e9a6
anuja-jmk <203ajmk@gmail.com>
2026-02-17 10:19:25 +0530

    calculator app

---
+++ calculator.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "1.Addition"
+echo "2.Subtraction"
+echo "3.Multiplication"
+echo "4:Division"
+
+echo -n "Enter First Number:"
+read a
+echo -n "Enter Second Number:"
+read b
+echo -n "Enter the choice:"
+read ch
+case $ch in
+ 1)res=`expr $a + $b`
+ ;;
+ 2)res=`expr $a - $b`
+ ;;
+ 3)res=`expr $a \* $b`
+ ;;
+ 4)res=`expr $a / $b`
+ ;;
+esac
+echo "Result:$res"
